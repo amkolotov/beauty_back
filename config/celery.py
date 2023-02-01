f8cf05e4879166c59276b1964930f8a5646ad389
amkolotov <a.kolotov@unwind.ru>
2023-02-01 14:56:27 +0500

    added celery for send push notifications

--- config/celery.py
+++ config/celery.py
@@ -1,30 +1,12 @@
 import os
 
-from celery import Celery, signals
+from celery import Celery
 
-from kombu import Queue, Exchange
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-app = Celery('config', include=['apps.salon.tasks'])
+app = Celery('config')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
-
-app.conf.broker_transport_options = {'visibility_timeout': 86400}
-
-app.conf.task_queues = (
-    Queue('main', Exchange('main'), routing_key='main'),
-)
-
-app.conf.task_routes = {
-    'send_push_notifications': {
-        'queue': 'main',
-    },
-}
-
-# @signals.setup_logging.connect
-# def on_celery_setup_logging(**kwargs):
-#     pass
-
